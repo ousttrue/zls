16bb4b73cf3caa91f0bc5f46325465a3d96e269b
ousttrue <ousttrue@gmail.com>
2022-07-21 19:19:56 +0900

    ast_get_children

--- pkgs/workspace/src/AstContext.zig
+++ pkgs/workspace/src/AstContext.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const Self = @This();
 const PositionContext = @import("./position_context.zig").PositionContext;
+const getChildren = @import("./ast_get_children.zig").getChildren;
 
 fn getAllTokens(allocator: std.mem.Allocator, source: [:0]const u8) std.ArrayList(std.zig.Token) {
     var tokens = std.ArrayList(std.zig.Token).init(allocator);
@@ -19,62 +20,6 @@ fn getAllTokens(allocator: std.mem.Allocator, source: [:0]const u8) std.ArrayLis
     }
 
     return tokens;
-}
-
-pub fn getChildren(children: *std.ArrayList(u32), tree: *const std.zig.Ast, idx: u32) void {
-    const tag = tree.nodes.items(.tag);
-    const node_tag = tag[idx];
-    const data = tree.nodes.items(.data);
-    const node_data = data[idx];
-
-    switch (node_tag) {
-        .simple_var_decl => {
-            const var_decl = tree.simpleVarDecl(idx);
-            if (var_decl.ast.type_node != 0) {
-                children.append(var_decl.ast.type_node) catch unreachable;
-            }
-            if (var_decl.ast.init_node != 0) {
-                children.append(var_decl.ast.init_node) catch unreachable;
-            }
-        },
-        .fn_decl => {
-            // fn_proto
-            children.append(node_data.lhs) catch unreachable;
-
-            // body
-            children.append(node_data.rhs) catch unreachable;
-        },
-        .builtin_call_two => {
-            if (node_data.lhs != 0) {
-                children.append(node_data.lhs) catch unreachable;
-            }
-            if (node_data.rhs != 0) {
-                children.append(node_data.rhs) catch unreachable;
-            }
-        },
-        .field_access => {
-            children.append(node_data.lhs) catch unreachable;
-        },
-        .string_literal => {
-            // leaf. no children
-        },
-        .block, .block_semicolon => {
-            for (tree.extra_data[node_data.lhs..node_data.rhs]) |child| {
-                children.append(child) catch unreachable;
-            }
-        },
-        .block_two, .block_two_semicolon => {
-            if (node_data.lhs != 0) {
-                children.append(node_data.lhs) catch unreachable;
-            }
-            if (node_data.rhs != 0) {
-                children.append(node_data.rhs) catch unreachable;
-            }
-        },
-        else => {
-            // std.debug.print("unknown node: {s}\n", .{@tagName(node_tag)});
-        },
-    }
 }
 
 pub fn traverse(context: *Self, stack: *std.ArrayList(u32)) void {
